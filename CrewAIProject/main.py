97b80adfadb62147ffa83f6a0b118fd5346fb485
Ritanshu Keshari <ritanshu.keshari@gmail.com>
2026-04-10 14:27:13 +0530

    Build EmailAgent with tasks with crew AI

--- CrewAIProject/main.py
+++ CrewAIProject/main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from crewaiproject!")
+from dotenv import load_dotenv
+from crewai import LLM
 
+def main():
+    load_dotenv()
+    print("Hello from crewaiproject!")
+    llm = LLM(model="google/gemini-2.5-flash-lite", temperature=0.1)
+    response = llm.call("Who invented bulb")
+    print(response)
 
 if __name__ == "__main__":
     main()
